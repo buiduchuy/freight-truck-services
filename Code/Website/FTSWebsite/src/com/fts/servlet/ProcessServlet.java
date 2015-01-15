dadf0eed7632918f7f5c99c281c91c1d175d7ff8
Bùi Đức Huy <duchuy.bui2108@gmail.com>
2015-01-16 01:20:22 +0700

    HUYBD: Create website

---
+++ Code/Website/FTSWebsite/src/com/fts/servlet/ProcessServlet.java
@@ -0,0 +1,22 @@
+package com.fts.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Huy on 1/16/2015.
+ */
+@WebServlet(name = "ProcessServlet")
+public class ProcessServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
