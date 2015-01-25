fbf3db3a6515c8b09e4c023adf2249593acda3fb
HuyBD <duchuy.bui2108@gmail.com>
2015-01-25 16:35:10 +0700

    HuyBD: Create 3 packages

---
+++ Code/Website/src/vn/edu/fpt/fts/dao/UserDao.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package vn.edu.fpt.fts.dao;
+
+/**
+ * @author Huy
+ *
+ */
+public class UserDao {
+
+}
---
+++ Code/Website/src/vn/edu/fpt/fts/model/User.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package vn.edu.fpt.fts.model;
+
+/**
+ * @author Huy
+ *
+ */
+public class User {
+
+}
