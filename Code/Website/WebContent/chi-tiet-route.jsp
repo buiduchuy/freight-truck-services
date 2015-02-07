366d7b2a018471d0032d5db7dcb9d182aa0d7f81
KhuongNguyen <nguyenromio@gmail.com>
2015-02-07 11:42:20 -0800

    Update Controller, chi-tiet-route

---
+++ Code/Website/WebContent/chi-tiet-route.jsp
@@ -0,0 +1,204 @@
+<%-- 
+    Document   : chi-tiet-route
+    Created on : Jan 30, 2015, 10:58:01 PM
+    Author     : KhuongNguyen-PC
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<title>Chi tiết tài xế</title>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<c:set var="detailRoute" value="${sessionScope.viewDetailRoute }" />
+<jsp:include page="header.jsp" />
+<style>
+html, body, #map-canvas {
+	height: 100%;
+	margin: 0px;
+	padding: 0px
+}
+
+#panel {
+	position: absolute;
+	top: 5px;
+	left: 50%;
+	margin-left: -180px;
+	z-index: 5;
+	background-color: #fff;
+	padding: 5px;
+	border: 1px solid #999;
+}
+</style>
+<script
+	src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
+<script>
+	var directionsDisplay;
+	var directionsService = new google.maps.DirectionsService();
+	var map;
+
+	function initialize() {
+		directionsDisplay = new google.maps.DirectionsRenderer();
+		var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+		var mapOptions = {
+			zoom : 7,
+			center : chicago
+		};
+		map = new google.maps.Map(document.getElementById('map-canvas'),
+				mapOptions);
+		directionsDisplay.setMap(map);
+	}
+
+	function calcRoute() {
+		var start = '533 Huỳnh Văn Bánh, 14, Phú Nhuận';
+		var end = 'Hanoi, Vietnam';
+		var request = {
+			origin : start,
+			destination : end,
+			travelMode : google.maps.TravelMode.DRIVING
+		};
+		directionsService.route(request, function(response, status) {
+			if (status == google.maps.DirectionsStatus.OK) {
+				directionsDisplay.setDirections(response);
+			}
+		});
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+	google.maps.event.addDomListener(window, 'load', calcRoute);
+</script>
+<style>
+#map-canvas {
+	width: 100%;
+	height: 300px;
+}
+</style>
+<section class="container">
+	<center>
+		<div class="form-content"
+			style="border: 1px solid #ccc; box-shadow: 1px 1px 2px 2px #CCC; margin-bottom: 50px; width: 800px;">
+
+			<div class="form-content">
+				<c:choose>
+					<c:when test="${not empty detailRoute }">
+						<form action="#" method="post" accept-charset="utf-8"
+							enctype="multipart/form-data" data-abide=""
+							novalidate="novalidate">
+							<div class="row">
+								<div class="large-12 columns">
+									<h2 class="page-title">
+										<font color="orange">Chi tiết lộ trình</font>
+									</h2>
+								</div>
+								<c:set var="error" value="${sessionScope.errorSendSuggest}" />
+								<c:if test="${not empty error}">
+									<font color="red">${error}</font>
+								</c:if>
+								<div class="large-12 columns">
+									<div data-alert="" class="alert-box radius secondary">
+										<label class="left"><font color="white" size="+1">Thông
+												tin lộ trình</font></label> </br>
+									</div>
+									<div class="row">
+										<div class="small-4 columns ">
+											<label class="right inline">Địa điểm bắt đầu:</label>
+										</div>
+										<div class="small-6 columns left">
+											<input type="text" id="startAddress" class="left inline"
+												value="${detailRoute.startingAddress }" readonly="readonly">
+										</div>
+									</div>
+									<div class="row">
+										<div class="small-4 columns ">
+											<label class="right inline">Thời gian bắt đầu:</label>
+										</div>
+										<div class="small-6 columns left">
+											<input type="text" class="left inline"
+												value="${detailRoute.startTime }" readonly="readonly">
+										</div>
+									</div>
+									<div class="row">
+										<div class="small-4 columns ">
+											<label class="right inline">Địa điểm kết thúc:</label>
+										</div>
+										<div class="small-6 columns left">
+											<input type="text" id="endAddress" class="left inline"
+												value="${detailRoute.destinationAddress }"
+												readonly="readonly">
+										</div>
+									</div>
+									<div class="row">
+										<div class="small-4 columns ">
+											<label class="right inline">Thời gian kết thúc:</label>
+										</div>
+										<div class="small-6 columns left">
+											<input type="text" class="left inline"
+												value="${detailRoute.finishTime }" readonly="readonly">
+										</div>
+									</div>
+									<div class="row">
+										<div class="small-4 columns ">
+											<label class="right inline">Khối lượng có thể chở:</label>
+										</div>
+										<div class="small-6 columns left">
+											<input type="text" class="left inline"
+												value="${detailRoute.weight } Kg" readonly="readonly">
+										</div>
+									</div>
+									<div class="row">
+										<div class="small-4 columns ">
+											<label class="right inline">Ghi chú:</label>
+										</div>
+										<div class="small-6 columns left">
+											<input type="text" class="left inline"
+												value="${detailRoute.notes }" readonly="readonly">
+										</div>
+									</div>
+
+
+
+
+									<div class="row">
+										<div class="submit-area">
+											<a href="goi-y-he-thong.jsp" class="button secondary"> <i
+												class="icon-mail-reply"></i> Trở về trước
+											</a> <a class="button success"
+												href="Controller?btnAction=sendSuggest&routeID=${detailRoute.routeID }">
+												<i class="icon-envelope"></i> Gửi để nghị
+											</a>
+
+										</div>
+
+										</br>
+
+									</div>
+
+								</div>
+								<div class="large-12 columns">
+									<div data-alert="" class="alert-box radius secondary">
+										<label class="left"><font color="white" size="+1">Lộ
+												trình</font></label> </br>
+									</div>
+									<div id="map-canvas"></div>
+									</br> </br>
+								</div>
+
+
+
+
+							</div>
+						</form>
+					</c:when>
+					<c:otherwise>
+						<jsp:forward page="goi-y-he-thong.jsp" />
+					</c:otherwise>
+				</c:choose>
+
+
+			</div>
+
+		</div>
+		</br>
+
+	</center>
+
+</section>
+<jsp:include page="footer.jsp" />
--- Code/Website/src/vn/edu/fpt/fts/servlet/Controller.java
+++ Code/Website/src/vn/edu/fpt/fts/servlet/Controller.java
@@ -1,14 +1,11 @@
 package vn.edu.fpt.fts.servlet;
 
-import java.io.PrintWriter;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,16 +14,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.sun.org.apache.bcel.internal.generic.ACONST_NULL;
-
 import vn.edu.fpt.fts.common.Common;
 import vn.edu.fpt.fts.dao.AccountDAO;
+import vn.edu.fpt.fts.dao.DealDAO;
 import vn.edu.fpt.fts.dao.DriverDAO;
 import vn.edu.fpt.fts.dao.GoodsCategoryDAO;
 import vn.edu.fpt.fts.dao.GoodsDAO;
 import vn.edu.fpt.fts.dao.OwnerDAO;
 import vn.edu.fpt.fts.dao.RouteDAO;
-import vn.edu.fpt.fts.pojo.Account;
+import vn.edu.fpt.fts.pojo.Deal;
 import vn.edu.fpt.fts.pojo.Driver;
 import vn.edu.fpt.fts.pojo.Goods;
 import vn.edu.fpt.fts.pojo.GoodsCategory;
@@ -55,10 +51,11 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, "abc");
 		response.setContentType("text/html;charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
-		RouteDAO rou= new RouteDAO();
+		RouteDAO rou = new RouteDAO();
+		DealDAO dea = new DealDAO();
+		GoodsDAO goodDao= new GoodsDAO();
 		String action = request.getParameter("btnAction");
 		HttpSession session = request.getSession(true);
 		if ("offAccount".equals(action)) {
@@ -84,13 +81,58 @@ public class Controller extends HttpServlet {
 			RequestDispatcher rd = request
 					.getRequestDispatcher("tao-hang-3.jsp");
 			rd.forward(request, response);
-		}if("viewDetailRouter".equals(action)){
-		int idRouter=Integer.parseInt(request.getParameter("idRouter"));
-		Route router= rou.getRouteById(idRouter);
-		session.setAttribute("viewDetailRoute", router);
-		RequestDispatcher rd = request
-				.getRequestDispatcher("chi-tiet-route.jsp");
-		rd.forward(request, response);
+		}
+		if ("viewDetailRouter".equals(action)) {
+			int idRouter = Integer.parseInt(request.getParameter("idRouter"));
+			Route router = rou.getRouteById(idRouter);
+			session.setAttribute("viewDetailRoute", router);
+			RequestDispatcher rd = request
+					.getRequestDispatcher("chi-tiet-route.jsp");
+			rd.forward(request, response);
+		}
+		if ("sendSuggest".equals(action)) {
+			if (session.getAttribute("viewDetailRoute") != null
+					&& session.getAttribute("newGood") != null) {
+				DateFormat dateFormat = new SimpleDateFormat(
+						"yyyy/MM/dd HH:mm:ss");
+				Date date = new Date();
+				String createTime = dateFormat.format(date);
+				Route route = (Route) session.getAttribute("viewDetailRoute");
+				Goods good = (Goods) session.getAttribute("newGood");
+				Deal deal = new Deal(good.getPrice(), good.getNotes(),
+						createTime, 0, "owner", route.getDriverID(),
+						good.getGoodsID(), 1);
+				if (dea.insertDeal(deal) == 1) {
+					session.setAttribute("messageCreateGood",
+							"Gửi đề nghị thành công !");
+					RequestDispatcher rd = request
+							.getRequestDispatcher("goi-y-he-thong.jsp");
+					rd.forward(request, response);
+				}
+
+			}
+		}
+		if ("manageGoods".equals(action)) {
+			Owner owner=(Owner) session.getAttribute("owner");
+			List<Goods> manageGood=goodDao.getListGoodsByOwnerID(owner);
+			List<Goods> manageGood1= new ArrayList<Goods>();
+			List<Goods> manageGood2= new ArrayList<Goods>();
+			for(int i=0;i<manageGood.size();i++){
+				if(manageGood.get(i).getActive()==1){
+					manageGood1.add(manageGood.get(i));
+				}else{
+					manageGood2.add(manageGood.get(i));
+				}
+			}
+			Goods[] list1= new Goods[manageGood1.size()];
+			manageGood1.toArray(list1);
+			Goods[] list2= new Goods[manageGood2.size()];
+			manageGood2.toArray(list2);
+			session.setAttribute("listGood1", list1);
+			session.setAttribute("listGood2", list2);
+			RequestDispatcher rd = request
+					.getRequestDispatcher("quan-ly-hang.jsp");
+			rd.forward(request, response);
 		}
 	}
 
@@ -98,10 +140,9 @@ public class Controller extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	
+
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, new RuntimeException());
 		// TODO Auto-generated method stub
 		response.setContentType("text/html;charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
@@ -109,34 +150,35 @@ public class Controller extends HttpServlet {
 		HttpSession session = request.getSession(true);
 		GoodsCategoryDAO goodCa = new GoodsCategoryDAO();
 		AccountDAO acc = new AccountDAO();
-		RouteDAO rou= new RouteDAO();
-		OwnerDAO ow= new OwnerDAO();
-		DriverDAO dri= new DriverDAO();
+		RouteDAO rou = new RouteDAO();
+		OwnerDAO ow = new OwnerDAO();
+		DriverDAO dri = new DriverDAO();
 		if ("login".equals(action)) {
 			String email = request.getParameter("txtEmail");
 			String password = request.getParameter("txtPassword");
 			session.removeAttribute("errorLogin");
 			session.removeAttribute("account");
-			if (acc.checkLoginAccount(email, password)!=null) {
-				
-				Owner owner= ow.getOwnerByEmail(acc.checkLoginAccount(email, password));
-				List<GoodsCategory> list= goodCa.getAllGoodsCategory();
-				GoodsCategory[] typeGoods= new GoodsCategory[list.size()];
+			if (acc.checkLoginAccount(email, password) != null) {
+
+				Owner owner = ow.getOwnerByEmail(acc.checkLoginAccount(email,
+						password));
+				List<GoodsCategory> list = goodCa.getAllGoodsCategory();
+
+				GoodsCategory[] typeGoods = new GoodsCategory[list.size()];
 				list.toArray(typeGoods);
 				session.setAttribute("typeGoods", typeGoods);
 				session.setAttribute("owner", owner);
 				session.setAttribute("account", owner.getLastName());
-				if(session.getAttribute("namePage")!=null){
+				if (session.getAttribute("namePage") != null) {
 					String prePage = (String) session.getAttribute("namePage");
-						RequestDispatcher rd = request
-								.getRequestDispatcher(prePage);
-						rd.forward(request, response);
-					}
-					else{
-						RequestDispatcher rd = request
-								.getRequestDispatcher("index.jsp");
-						rd.forward(request, response);
-					}
+					RequestDispatcher rd = request
+							.getRequestDispatcher(prePage);
+					rd.forward(request, response);
+				} else {
+					RequestDispatcher rd = request
+							.getRequestDispatcher("index.jsp");
+					rd.forward(request, response);
+				}
 			} else {
 				session.setAttribute("errorLogin",
 						"Email hoặc mật khẩu không đúng. Xin đăng nhập lại !");
@@ -150,7 +192,6 @@ public class Controller extends HttpServlet {
 			String pickupTime = request.getParameter("txtpickupTime");
 			String deliveryAddress = request.getParameter("txtdeliveryAddress");
 			String deliveryTime = request.getParameter("txtdeliveryTime");
-		
 
 			Goods r = new Goods(pickupTime, pickupAddress, deliveryTime,
 					deliveryAddress);
@@ -166,7 +207,7 @@ public class Controller extends HttpServlet {
 			String pickupTime = request.getParameter("txtpickupTime");
 			String deliveryAddress = request.getParameter("txtdeliveryAddress");
 			String deliveryTime = request.getParameter("txtdeliveryTime");
-	
+
 			Goods r = new Goods(pickupTime, pickupAddress, deliveryTime,
 					deliveryAddress);
 			session.setAttribute("router", r);
@@ -274,11 +315,11 @@ public class Controller extends HttpServlet {
 				RequestDispatcher rd = request
 						.getRequestDispatcher("tao-hang-3.jsp");
 				rd.forward(request, response);
-			} 
-			Common co= new Common();
+			}
+			Common co = new Common();
 			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 			Date date = new Date();
-			String createTime=dateFormat.format(date);
+			String createTime = dateFormat.format(date);
 			String pickupAdress = ((Goods) session.getAttribute("router"))
 					.getPickupAddress();
 			String deliveryAddress = ((Goods) session.getAttribute("router"))
@@ -292,25 +333,33 @@ public class Controller extends HttpServlet {
 					.getGoodsCategoryID();
 			String notes = ((Goods) session.getAttribute("good")).getNotes();
 			int price = (int) session.getAttribute("price");
-			float a= Float.parseFloat("10");
-			Owner owner= (Owner) session.getAttribute("owner");
-			Goods goo= new Goods(weight, price, co.changeFormatDate(pickupTime),  pickupAdress, co.changeFormatDate(deliveryTime),  deliveryAddress, a, a, a, a, notes, createTime, 1, owner.getOwnerID(), GoodsCategoryID);
-			GoodsDAO goodDao= new GoodsDAO();
-			List<Route> list= rou.getAllRoute();
-			Route[] listRou= new Route[list.size()];
+			float a = Float.parseFloat("10");
+			Owner owner = (Owner) session.getAttribute("owner");
+			Goods goo = new Goods(weight, price,
+					co.changeFormatDate(pickupTime), pickupAdress,
+					co.changeFormatDate(deliveryTime), deliveryAddress, a, a,
+					a, a, notes, createTime, 1, owner.getOwnerID(),
+					GoodsCategoryID);
+			GoodsDAO goodDao = new GoodsDAO();
+			List<Route> list = rou.getAllRoute();
+			Route[] listRou = new Route[list.size()];
 			list.toArray(listRou);
-			List<Driver> listDriver= dri.getAllDriver();
-			Driver[] listDri= new Driver[listDriver.size()];
+			List<Driver> listDriver = dri.getAllDriver();
+			Driver[] listDri = new Driver[listDriver.size()];
 			listDriver.toArray(listDri);
-			if(goodDao.insertGoods(goo)==1){
+			if (goodDao.insertGoods(goo) == 1) {
+				List<Goods> listGood = goodDao.getListGoodsByOwnerID(owner);
+				Goods goolast = listGood.get(listGood.size() - 1);
+				session.setAttribute("newGood", goolast);
 				session.setAttribute("listRouter", listRou);
 				session.setAttribute("listDriver", listDri);
 				session.setAttribute("messageCreateGood", "Tạo hàng thành công");
 				RequestDispatcher rd = request
 						.getRequestDispatcher("goi-y-he-thong.jsp");
 				rd.forward(request, response);
-			}else{
-				session.setAttribute("errorCreateGood", "Có lỗi khi tạo hàng. Vui lòng thử lại");
+			} else {
+				session.setAttribute("errorCreateGood",
+						"Có lỗi khi tạo hàng. Vui lòng thử lại");
 				RequestDispatcher rd = request
 						.getRequestDispatcher("tao-hang-4.jsp");
 				rd.forward(request, response);
@@ -320,3 +369,4 @@ public class Controller extends HttpServlet {
 	}
 
 }
+
