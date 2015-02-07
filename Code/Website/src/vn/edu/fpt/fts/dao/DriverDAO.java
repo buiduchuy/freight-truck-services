be0f5d475b7cec8beae66bb64720cfdd1e2a2b52
KhuongNguyen <nguyenromio@gmail.com>
2015-02-07 04:51:44 -0800

    update driver and driverDao(with function get driver by id to view point in "Goi y He thong" page)

--- Code/Website/src/vn/edu/fpt/fts/dao/DriverDAO.java
+++ Code/Website/src/vn/edu/fpt/fts/dao/DriverDAO.java
@@ -3,10 +3,79 @@
  */
 package vn.edu.fpt.fts.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import vn.edu.fpt.fts.common.DBAccess;
+import vn.edu.fpt.fts.model.Driver;
+
 /**
  * @author Huy
  *
  */
 public class DriverDAO {
+	public Driver getDriverById(int Id) {
+		Connection con = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			con = DBAccess.makeConnection();
 
+			String sql = "SELECT * FROM [Driver] WHERE DriverId=?";
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, Id);
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				int driverID = rs.getInt("DriverID");
+				String email = rs.getString("Email");
+				String firstName = rs.getString("FirstName");
+				String lastName = rs.getString("LastName");
+				int sex = rs.getInt("Sex");
+				String phone = rs.getString("Phone");
+				boolean isActive = rs.getBoolean("IsActive");
+				String createBy = rs.getString("CreateBy");
+				String createTime = rs.getString("CreateTime");
+				String updateBy = rs.getString("UpdateBy");
+				String updateTime = rs.getString("UpdateTime");
+				int age = rs.getInt("Age");
+				String image = rs.getString("Image");
+				int point = rs.getInt("Point");
+				Driver driver = new Driver(driverID, email, firstName,
+						lastName, sex, phone, isActive, createBy, createTime,
+						updateBy, updateTime, age, image, point);
+				return driver;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Logger.getLogger(OwnerDAO.class.getName()).log(Level.SEVERE, null,
+					e);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			System.out.println("Columns with Integer type are null");
+			Logger.getLogger(OwnerDAO.class.getName()).log(Level.SEVERE, null,
+					e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				Logger.getLogger(OwnerDAO.class.getName()).log(Level.SEVERE,
+						null, e);
+			}
+		}
+		return null;
+	}
 }
--- Code/Website/src/vn/edu/fpt/fts/model/Driver.java
+++ Code/Website/src/vn/edu/fpt/fts/model/Driver.java
@@ -8,5 +8,126 @@ package vn.edu.fpt.fts.model;
  *
  */
 public class Driver {
+	private int DriverID;
+	private String Email;
+	private String FirstName;
+	private String LastName;
+	private int Sex;
+	private String Phone;
+	private boolean IsActive;
+	private String CreateBy;
+	private String CreateTime;
+	private String UpdateBy;
+	private String UpdateTime;
+	private int Age;
+	private String Image;
+	private int Point;
+	public Driver() {
+		super();
+	}
+	public Driver(int driverID, String email, String firstName,
+			String lastName, int sex, String phone, boolean isActive,
+			String createBy, String createTime, String updateBy,
+			String updateTime, int age, String image, int point) {
+		super();
+		DriverID = driverID;
+		Email = email;
+		FirstName = firstName;
+		LastName = lastName;
+		Sex = sex;
+		Phone = phone;
+		IsActive = isActive;
+		CreateBy = createBy;
+		CreateTime = createTime;
+		UpdateBy = updateBy;
+		UpdateTime = updateTime;
+		Age = age;
+		Image = image;
+		Point = point;
+	}
+	public int getDriverID() {
+		return DriverID;
+	}
+	public void setDriverID(int driverID) {
+		DriverID = driverID;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public int getSex() {
+		return Sex;
+	}
+	public void setSex(int sex) {
+		Sex = sex;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public boolean isIsActive() {
+		return IsActive;
+	}
+	public void setIsActive(boolean isActive) {
+		IsActive = isActive;
+	}
+	public String getCreateBy() {
+		return CreateBy;
+	}
+	public void setCreateBy(String createBy) {
+		CreateBy = createBy;
+	}
+	public String getCreateTime() {
+		return CreateTime;
+	}
+	public void setCreateTime(String createTime) {
+		CreateTime = createTime;
+	}
+	public String getUpdateBy() {
+		return UpdateBy;
+	}
+	public void setUpdateBy(String updateBy) {
+		UpdateBy = updateBy;
+	}
+	public String getUpdateTime() {
+		return UpdateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		UpdateTime = updateTime;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public String getImage() {
+		return Image;
+	}
+	public void setImage(String image) {
+		Image = image;
+	}
+	public int getPoint() {
+		return Point;
+	}
+	public void setPoint(int point) {
+		Point = point;
+	}
 
 }
