97c6f1869d470bde0f7fa265ac92e1f3a66f5806
Bui Duc Huy <duchuy.bui2108@gmail.com>
2015-02-08 08:19:27 +0700

    Fix OnwerDAO, update database

--- Code/Website/src/vn/edu/fpt/fts/model/Role.java
+++ Code/Website/src/vn/edu/fpt/fts/model/Role.java
@@ -1,5 +1,12 @@
 package vn.edu.fpt.fts.model;
 
+/**
+ * @author Huy
+ *
+ */
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Role implements java.io.Serializable {
 
 	/**
@@ -8,11 +15,11 @@ public class Role implements java.io.Serializable {
 	private static final long serialVersionUID = 6383031919620384310L;
 	private int roleId;
 	private String roleName;
-	
+
 	public Role() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public Role(int roleId, String roleName) {
 		super();
 		this.roleId = roleId;
@@ -22,12 +29,15 @@ public class Role implements java.io.Serializable {
 	public int getRoleId() {
 		return roleId;
 	}
+
 	public void setRoleId(int roleId) {
 		this.roleId = roleId;
 	}
+
 	public String getRoleName() {
 		return roleName;
 	}
+
 	public void setRoleName(String roleName) {
 		this.roleName = roleName;
 	}
