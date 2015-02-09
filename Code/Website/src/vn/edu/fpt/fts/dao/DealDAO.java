6382f64c521e21e17d90102b74fbd851e8ca2a5d
Bui Duc Huy <duchuy.bui2108@gmail.com>
2015-02-09 12:01:05 +0700

    Update dao, database, api

--- Code/Website/src/vn/edu/fpt/fts/dao/DealDAO.java
+++ Code/Website/src/vn/edu/fpt/fts/dao/DealDAO.java
@@ -5,7 +5,10 @@ package vn.edu.fpt.fts.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,17 +32,15 @@ public class DealDAO {
 			con = DBAccess.makeConnection();
 
 			String sql = "INSERT INTO Deal ( " + "Price," + "Notes,"
-					+ "CreateTime," + "OrderID," + "Sender," + "RouteID,"
-					+ "GoodsID," + "DealStatusID," + "Active" + ") VALUES ("
-					+ "?, " + "?, " + "?, " + "?, " + "?, " + "?, " + "?, "
-					+ "?, " + "?)";
+					+ "CreateTime," + "Sender," + "RouteID," + "GoodsID,"
+					+ "DealStatusID," + "Active" + ") VALUES (" + "?, " + "?, "
+					+ "?, " + "?, " + "?, " + "?, " + "?, " + "?)";
 			stmt = con.prepareStatement(sql);
 			int i = 1;
 
 			stmt.setDouble(i++, bean.getPrice()); // Price
 			stmt.setString(i++, bean.getNotes()); // Notes
 			stmt.setString(i++, bean.getCreateTime()); // CreateTime
-			stmt.setInt(i++, bean.getOrderID()); // OrderID
 			stmt.setString(i++, bean.getSender()); // Sender
 			stmt.setInt(i++, bean.getRouteID()); // RouteID
 			stmt.setInt(i++, bean.getGoodsID()); // GoodsID
@@ -69,6 +70,122 @@ public class DealDAO {
 			}
 		}
 		return ret;
+	}
+
+	public List<Deal> getDealByGoodsID(int goodsId) {
+		Connection con = null;
+		PreparedStatement stm = null;
+		ResultSet rs = null;
+		try {
+			con = DBAccess.makeConnection();
+
+			String sql = "SELECT * FROM Deal WHERE GoodsID=?";
+
+			stm = con.prepareStatement(sql);
+
+			int i = 1;
+			stm.setInt(i++, goodsId);
+
+			rs = stm.executeQuery();
+			List<Deal> list = new ArrayList<Deal>();
+			Deal deal;
+
+			while (rs.next()) {
+				deal = new Deal();
+
+				deal.setDealID(rs.getInt("DealID"));
+				deal.setPrice(rs.getDouble("Price"));
+				deal.setNotes(rs.getString("Notes"));
+				deal.setCreateTime(rs.getString("CreateTime"));
+				deal.setSender(rs.getString("Sender"));
+				deal.setRouteID(rs.getInt("RouteID"));
+				deal.setGoodsID(rs.getInt("GoodsID"));
+				deal.setDealStatusID(rs.getInt("DealStatusID"));
+				deal.setActive(rs.getInt("Active"));
+
+				list.add(deal);
+			}
+			return list;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Logger.getLogger(TAG).log(Level.SEVERE, null, e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (stm != null) {
+					stm.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("Can't load data from Deal table");
+				Logger.getLogger(TAG).log(Level.SEVERE, null, e);
+			}
+		}
+		return null;
+	}
+
+	public List<Deal> getDealByRouteID(int routeID) {
+		Connection con = null;
+		PreparedStatement stm = null;
+		ResultSet rs = null;
+		try {
+			con = DBAccess.makeConnection();
+
+			String sql = "SELECT * FROM Deal WHERE RouteID=?";
+
+			stm = con.prepareStatement(sql);
+
+			int i = 1;
+			stm.setInt(i++, routeID);
+
+			rs = stm.executeQuery();
+			List<Deal> list = new ArrayList<Deal>();
+			Deal deal;
+
+			while (rs.next()) {
+				deal = new Deal();
+
+				deal.setDealID(rs.getInt("DealID"));
+				deal.setPrice(rs.getDouble("Price"));
+				deal.setNotes(rs.getString("Notes"));
+				deal.setCreateTime(rs.getString("CreateTime"));
+				deal.setSender(rs.getString("Sender"));
+				deal.setRouteID(rs.getInt("RouteID"));
+				deal.setGoodsID(rs.getInt("GoodsID"));
+				deal.setDealStatusID(rs.getInt("DealStatusID"));
+				deal.setActive(rs.getInt("Active"));
+
+				list.add(deal);
+			}
+			return list;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Logger.getLogger(TAG).log(Level.SEVERE, null, e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (stm != null) {
+					stm.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("Can't load data from Deal table");
+				Logger.getLogger(TAG).log(Level.SEVERE, null, e);
+			}
+		}
+		return null;
 	}
 
 }
