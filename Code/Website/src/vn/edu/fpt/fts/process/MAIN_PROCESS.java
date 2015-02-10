69d650c24dc0998d5a9a0db3ce3f00874837f016
Bui Duc Huy <duchuy.bui2108@gmail.com>
2015-02-10 11:14:27 +0700

    Test matching process

--- Code/Website/src/vn/edu/fpt/fts/process/MAIN_PROCESS.java
+++ Code/Website/src/vn/edu/fpt/fts/process/MAIN_PROCESS.java
@@ -14,6 +14,26 @@ public class MAIN_PROCESS {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MatchingProcess matching = new MatchingProcess();
+		
+		// Vung Tau
+		// Double latGoods = 10.4025053;
+		// Double longGoods = 107.1255859;
+
+		// Quang Trung, TP.HCM
+		Double latGoods = 10.853132;
+		Double longGoods = 106.626289;
+
+		// Quang Trung, TP.HCM
+		Double latStart = 10.853132;
+		Double longStart = 106.626289;
+
+		// Xuan Loc, Dong Nai
+		Double latEnd = 10.9234099;
+		Double longEnd = 107.4084806;
+
+		System.out.println(matching.checkDistance(latGoods, longGoods,
+				latStart, longStart, latEnd, longEnd));
 
 	}
 
