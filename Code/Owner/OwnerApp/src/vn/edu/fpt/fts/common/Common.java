f042f2b1ed076730c8156c66c2fdd81c65747e03
Jay Dang <jaydang76@gmail.com>
2015-02-11 18:35:40 +0700

    update owner app

--- Code/Owner/OwnerApp/src/vn/edu/fpt/fts/common/Common.java
+++ Code/Owner/OwnerApp/src/vn/edu/fpt/fts/common/Common.java
@@ -1,11 +1,11 @@
 package vn.edu.fpt.fts.common;
 
 public final class Common {
-	public static final String IP_URL = "http://192.168.43.189:8080/FTS/api/";
+	public static final String IP_URL = "http://192.168.43.86:8080/FTS/api/";
 	public static final String Service_Goods_Create = "Goods/Create";
-	public static final String Service_Login = "Account/Login";
+	public static final String Service_Login = "Account/OwnerLogin";
 	public static final String Service_GoodsCategory_Get = "GoodsCategory/get";
 	public static final String Service_Route_Get = "Route/get";
-	public static final String Service_Goods_Get = "Goods/get";
-	
+	public static final String Service_Goods_Get = "Goods/getListGoodsByOwnerID";
+	public static final String Service_Route_GetByID = "Route/getRouteByID";
 }
