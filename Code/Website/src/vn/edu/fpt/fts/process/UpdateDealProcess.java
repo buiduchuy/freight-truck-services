03336ec8a6b1c1c730fc846ab63bc2d7ceefdf25
Bui Duc Huy <duchuy.bui2108@gmail.com>
2015-02-11 14:00:57 +0700

    Update dealDAO

--- Code/Website/src/vn/edu/fpt/fts/process/UpdateDealProcess.java
+++ Code/Website/src/vn/edu/fpt/fts/process/UpdateDealProcess.java
@@ -15,10 +15,38 @@ import vn.edu.fpt.fts.pojo.Deal;
  */
 public class UpdateDealProcess {
 
-	DealDAO dealDao = new DealDAO();
+	public static void main(String[] args) {
 
-	List<Deal> listDeal = new ArrayList<Deal>();
-	
-	
+		changeDealOwnerAccept(1);
+	}
+
+	public static int changeDealOwnerAccept(int ownerID) {
+		DealDAO dealDao = new DealDAO();
+		Deal deal = new Deal();
+		List<Deal> listDeal = new ArrayList<Deal>();
+		List<Deal> l_declineDeal = new ArrayList<Deal>();
+		// bean.setDealStatusID(3);
+
+		int s_update = 1;
+		// DealDAO.updateDeal(bean);
+
+		if (s_update != 0) {
+			listDeal = dealDao.getDealByOwnerID(ownerID);
+			for (int i = 0; i < listDeal.size(); i++) {
+				if (listDeal.get(i).getDealStatusID() != 3) {
+
+					deal = listDeal.get(i);
+					l_declineDeal.add(deal);
+
+				}
+			}
+		}
+
+		return 1;
+	}
+
+	public static int updateDeal() {
+		return 1;
+	}
 
 }
