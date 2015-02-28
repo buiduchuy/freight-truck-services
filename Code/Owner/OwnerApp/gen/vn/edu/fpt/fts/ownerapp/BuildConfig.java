fe90cca287f4674bb43994ca95f4b0ecdc7487c7
Bui Duc Huy <duchuy.bui2108@gmail.com>
2015-02-28 17:38:41 +0700

    Update Owner App

--- Code/Owner/OwnerApp/gen/vn/edu/fpt/fts/ownerapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package vn.edu.fpt.fts.ownerapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Code/Owner/OwnerApp/src/vn/edu/fpt/fts/common/Common.java
+++ Code/Owner/OwnerApp/src/vn/edu/fpt/fts/common/Common.java
@@ -1,7 +1,8 @@
 package vn.edu.fpt.fts.common;
 
 public final class Common {
-	public static final String IP_URL = "http://192.168.43.10:8080/FTS/api/";
+//	public static final String IP_URL = "http://192.168.43.10:8080/FTS/api/";
+	public static final String IP_URL = "http://137.116.128.218:8080/FTS/api/";
 	public static final String Service_Goods_Create = "Goods/Create";
 	public static final String Service_Login = "Account/OwnerLogin";
 	public static final String Service_GoodsCategory_Get = "GoodsCategory/get";
