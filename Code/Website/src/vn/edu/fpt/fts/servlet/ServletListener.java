499b724434815bd3e2854fdfb29af72a8785143c
Duc Huy <duchuy.bui2108@gmail.com>
2015-04-11 11:57:35 +0700

    Update ServletListener

--- Code/Website/src/vn/edu/fpt/fts/servlet/ServletListener.java
+++ Code/Website/src/vn/edu/fpt/fts/servlet/ServletListener.java
@@ -25,41 +25,10 @@ public class ServletListener implements ServletContextListener {
 		Timer timer = new Timer();
 		timer.schedule(ftsTimer, 1000, (15 * 1000));
 
-		// ScheduledExecutorService exec = Executors
-		// .newSingleThreadScheduledExecutor();
-		// exec.scheduleAtFixedRate(new Runnable() {
-		// @Override
-		// public void run() {
-		// // do stuff
-		// DateFormat dateFormat = new SimpleDateFormat(
-		// "yyyy/MM/dd HH:mm:ss");
-		// Date date = new Date();
-		// System.out.println("-------" + dateFormat.format(date)
-		// + "-------");
-		//
-		// orderProcess.checkDelivery();
-		// }
-		// }, 0, 60, TimeUnit.SECONDS);
 	}
 
 	public void contextDestroyed(ServletContextEvent arg0) {
 		System.out.println("Servlet Context is destroyed....");
-		// Logger logger = Logger.getLogger("CleanupContextListener");
-		// Enumeration<Driver> drivers = DriverManager.getDrivers();
-		// while (drivers.hasMoreElements()) {
-		// Driver driver = drivers.nextElement();
-		// ClassLoader driverclassLoader = driver.getClass().getClassLoader();
-		// ClassLoader thisClassLoader = this.getClass().getClassLoader();
-		// if (driverclassLoader != null && thisClassLoader != null
-		// && driverclassLoader.equals(thisClassLoader)) {
-		// try {
-		// logger.warning("Deregistering: " + driver);
-		// DriverManager.deregisterDriver(driver);
-		// } catch (SQLException e) {
-		// e.printStackTrace();
-		// }
-		// }
-		// }
 	}
 
 	class FTSTimerTask extends TimerTask {
